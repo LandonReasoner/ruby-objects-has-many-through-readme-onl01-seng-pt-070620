541ecc8d6d4bb5607d342b59debf15f7f90e58b7
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-11 23:23:24 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,23 @@
 class Meal
+  
+  attr_accessor :total, :tip
+  attr_reader :waiter, :customer
+  
+  @@all = []
+  
+  def initialize(total, tip, waiter, customer)
+    @total = total
+    @tip = tip
+    @waiter = waiter
+    save
+  end
+  
+  def save 
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
